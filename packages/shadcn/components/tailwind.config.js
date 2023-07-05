02590eb96d7c4a07c5dcfaa1ae881a75a9448470
Saul <saul.moro.gomez@gmail.com>
2023-07-05 11:31:55 +0200

    feat(shadcn): initial packages

--- packages/shadcn/components/tailwind.config.js
+++ packages/shadcn/components/tailwind.config.js
@@ -1,12 +1,12 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
 
-const baseConfig = require('../../tailwind.config');
+const baseConfig = require('../../../tailwind.config');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [baseConfig],
-  content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'), ...createGlobPatternsForDependencies(__dirname)],
+  content: [join(__dirname, '/**/!(*.stories|*.spec).{ts,html}'), ...createGlobPatternsForDependencies(__dirname)],
   theme: {
     extend: {},
   },
---
+++ packages/shadcn/components/vite.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  cacheDir: '../../../node_modules/.vite/shadcn-components',
+
+  plugins: [
+    viteTsConfigPaths({
+      root: '../../../',
+    }),
+  ],
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['test-setup.ts'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    cache: {
+      dir: '../../../node_modules/.vitest',
+    },
+  },
+});
